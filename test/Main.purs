ec52f0efc72908c30e2da8d289c899d3962567b4
John Watson <john.watson@gmx.co.uk>
2017-10-08 17:31:51 +0100

    Added unit test framework

--- test/Main.purs
+++ test/Main.purs
@@ -2,13 +2,34 @@ module Test.Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log, logShow)
+import Control.Monad.Free (Free)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Aff.AVar (AVAR)
 
-import Data.SoundFont.Gleitz (gleitzNoteName)
+import Test.Unit (TestF, suite, test)
+import Test.Unit.Main (runTest)
+import Test.Unit.Console (TESTOUTPUT)
+import Test.Unit.Assert as Assert
+import Data.SoundFont.Gleitz (midiPitch)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "gleitz note name Bb0:"
-  logShow $ gleitzNoteName "Bb0"
-  log "gleitz note name C8:"
-  logShow $ gleitzNoteName "C8"
+gleitzSuite :: forall t. Free (TestF t) Unit
+gleitzSuite =
+  suite "gleitz" do
+    test "midi pitch Bb0" do
+      Assert.equal 10 (midiPitch "Bb0")
+    test "midi pitch A3" do
+      Assert.equal 45 (midiPitch "A3")
+    test "midi pitch C8" do
+      Assert.equal 96 (midiPitch "C8")
+
+main :: forall t.
+        Eff
+          ( console :: CONSOLE
+          , testOutput :: TESTOUTPUT
+          , avar :: AVAR
+          | t
+          )
+          Unit
+main = runTest do
+  suite "soundfonts" do
+    gleitzSuite
